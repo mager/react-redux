4cca31ed8429557d98fafa0c7c86ab49163afd48
Andrew Mager <mager@postmates.com>
2016-02-14 15:23:29 -0800

    YouTube hit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const YOUTUBE_API_KEY = 'AIzaSyAystcfyntHOXOf2qJRv1EljE59734-DIg';
 
-// Create a new component. This component should produce some HTML.
+YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
 const App = () => {
   return (
     <div>
@@ -14,5 +16,4 @@ const App = () => {
   );
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM).
 ReactDOM.render(<App />, document.querySelector('.container'));

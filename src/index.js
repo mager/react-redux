52f16cfa8e8d58b554e5402734321c7fca776dfe
Andrew Mager <mager@postmates.com>
2016-02-14 15:25:35 -0800

    Class-based App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
@@ -8,12 +8,14 @@ YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, function(data) {
   console.log(data);
 });
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));

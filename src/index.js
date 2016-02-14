4aa914a9f92df3efbb9573bfe140268e61b15782
Andrew Mager <mager@postmates.com>
2016-02-14 15:29:47 -0800

    Learning ES6

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const YOUTUBE_API_KEY = 'AIzaSyAystcfyntHOXOf2qJRv1EljE59734-DIg';
 
-YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos });
+    });
+  }
+
   render() {
     return (
       <div>
